54a86ddd725111af20a7bd6216fb56eb5cfe811f
vloparevich <vadim.loparevich@gmail.com>
2021-08-21 12:35:26 -0700

    Implement CRUD for drones

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model');
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  Drone.find().then((dronesFromDB) => {
+    console.log(dronesFromDB);
+    res.render('drones/list', { dronesFromDB });
+  });
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render('drones/create-form');
 });
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const dataFromForm = req.body;
+  Drone.create(dataFromForm)
+    .then((addedDrone) => {
+      console.log('This is a new Drone: ', addedDrone);
+      res.redirect('/drones');
+      // throw new Error('BROKEN')
+    })
+    .catch((err) => {
+      console.log('Drone was not added to DB', err);
+      res.redirect('/drones/create');
+    });
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-});
-
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const droneId = req.params.id;
+  console.log(droneId);
+  Drone.findById(droneId)
+    .then((foundDrone) => {
+      console.log(foundDrone);
+      res.render('drones/update-form', foundDrone);
+    })
+    .catch((err) => {
+      console.log(`Drone was not found: ${err}`);
+    });
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  console.log(`To be deleted ${req.params.id}`);
+  Drone.findOneAndDelete(req.params.id)
+    .then((deletedDocument) => {
+      console.log(
+        `Successfully deleted document that had the form: ${deletedDocument}.`
+      );
+      res.redirect('/drones');
+    })
+    .catch((err) =>
+      console.error(`Failed to find and delete document: ${err}`)
+    );
 });
 
+router.post('/drones/:id/edit', (req, res, next) => {
+  Drone.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+  }).then((updatedDrone) => {
+    console.log(updatedDrone);
+    res.redirect('/drones');
+  });
+});
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+require('../db/index');
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+];
+
+Drone.create(drones)
+  .then((responseFromDB) => {
+    console.log(`Drones were successfully added to the DB ${responseFromDB}`);
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(
+      `Something bad has happened during the storing drones into the DB ${err}`
+    );
+  });
